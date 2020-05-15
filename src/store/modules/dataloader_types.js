1f45f5a1a8594c816949d71a964602970093fe57
fangyang <fangyang.jing@hotmail.com>
2020-05-15 19:35:44 +0800

    [Update] Dataloader相关

--- src/store/modules/dataloader_types.js
+++ src/store/modules/dataloader_types.js
@@ -1,8 +1,12 @@
-
 export default {
-    name:'dataloader',
-    __init__:'__init__',
-    send:'send',
-    set_error:'set_error',
-    disconnect:'disconnect',
-}
+  name: "dataloader",
+  __init__: "__init__",
+  send: "send",
+  set_error: "set_error",
+  disconnect: "disconnect",
+  updateTimeFormat: "updateTimeFormat",
+  updateContractType: "updateContractType",
+  changeExchanges: "changeExchanges",
+  changeSymbols: "changeSymbols",
+  changePeriods: "changePeriods"
+};
